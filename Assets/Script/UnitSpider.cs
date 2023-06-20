867115b92cb5fe1950c85ce3479d970e7582e706
J4cK <lahuycong2000@gmail.com>
2023-06-20 18:46:41 +0700

    -fix: Scene

--- Assets/Script/UnitSpider.cs
+++ Assets/Script/UnitSpider.cs
@@ -11,19 +11,19 @@ public class UnitSpider : MonoBehaviour
     [SerializeField] private SpriteAtlas _spriteSpider;
     private List<Vector3> _listPosSpawn;
     private int _indexSpider = 10;
-    public int time = 0;
+    // public int time = 0;
     public void RandomSpider(int index)
     {
         string path = string.Format(PATH_ITEM, index, _indexSpider);
         _imageSpider.sprite = _spriteSpider.GetSprite(path);
     }
-    private void Update()
-    {
-        time++;
-        if (time > 1000)
-        {
-            GameManager.Instance.ReturnSpider(this);
-            time = 0;
-        }
-    }
+    // private void Update()
+    // {
+    //     time++;
+    //     if (time > 1000)
+    //     {
+    //         GameManager.Instance.ReturnSpider(this);
+    //         time = 0;
+    //     }
+    // }
 }
